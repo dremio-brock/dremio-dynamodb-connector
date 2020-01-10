551de20cdcd036475dc2ebdcb9dcd50488a43408
Brock Griffey <brock@dremio.com>
2020-01-10 13:44:14 -0500

    initial comming

---
+++ src/main/java/com/dremio/exec/store/jdbc/conf/DynamodbConf.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2017-2018 Dremio Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dremio.exec.store.jdbc.conf;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.dremio.exec.catalog.conf.DisplayMetadata;
+import com.dremio.exec.catalog.conf.NotMetadataImpacting;
+import com.dremio.exec.catalog.conf.SourceType;
+import com.dremio.exec.server.SabotContext;
+import com.dremio.exec.store.jdbc.CloseableDataSource;
+import com.dremio.exec.store.jdbc.DataSources;
+import com.dremio.exec.store.jdbc.JdbcStoragePlugin;
+import com.dremio.exec.store.jdbc.JdbcStoragePlugin.Config;
+import com.dremio.exec.store.jdbc.dialect.arp.ArpDialect;
+import com.google.common.annotations.VisibleForTesting;
+
+import io.protostuff.Tag;
+
+/**
+ * Configuration for SQLite sources.
+ */
+@SourceType(value = "DynamoDB", label = "DynamoDB")
+public class DynamodbConf extends AbstractArpConf<DynamodbConf> {
+  private static final String ARP_FILENAME = "arp/implementation/dynamodb-arp.yaml";
+  private static final ArpDialect ARP_DIALECT =
+      AbstractArpConf.loadArpFile(ARP_FILENAME, (ArpDialect::new));
+  private static final String DRIVER = "com.simba.dynamodb.jdbc.Driver";
+
+  @NotBlank
+  @Tag(1)
+  @DisplayMetadata(label = "Host")
+  public String host;
+
+  @NotBlank
+  @Tag(2)
+  @DisplayMetadata(label = "Access Key")
+  public String accesskey;
+
+  @NotBlank
+  @Tag(3)
+  @DisplayMetadata(label = "Secrect Key")
+  public String secrectkey;
+
+  @NotBlank
+  @Tag(4)
+  @DisplayMetadata(label = "Region")
+  public String region;
+
+  //@NotBlank
+  //@Tag(5)
+  //@DisplayMetadata(label = "Local Metadata File")
+  //public String localmetadatafile;
+
+  @VisibleForTesting
+  public String toJdbcConnectionString() {
+    final String host = checkNotNull(this.host, "Missing Host.");
+
+    final String accesskey = checkNotNull(this.accesskey, "Missing Access Key.");
+
+    final String secrectkey = checkNotNull(this.secrectkey, "Missing Secrect Key.");
+
+    final String region = checkNotNull(this.region, "Missing Region.");
+
+    //final String localmetadatafile = checkNotNull(this.localmetadatafile, "Missing Local Metadata File.");
+
+    //return String.format("jdbc:dynamodb:Host=%s;AccessKey=%s;SecretKey=%s;Region=%s;LocalMetadataFile=%s", host, accesskey, secrectkey, region, localmetadatafile);
+
+    return String.format("jdbc:dynamodb:Host=%s;AccessKey=%s;SecretKey=%s;Region=%s;", host, accesskey, secrectkey, region);
+  }
+
+  @Override
+  @VisibleForTesting
+  public Config toPluginConfig(SabotContext context) {
+    return JdbcStoragePlugin.Config.newBuilder()
+        .withDialect(getDialect())
+        .withDatasourceFactory(this::newDataSource)
+        .clearHiddenSchemas()
+        .addHiddenSchema("SYSTEM")
+        .build();
+  }
+
+  private CloseableDataSource newDataSource() {
+    return DataSources.newGenericConnectionPoolDataSource(DRIVER,
+      toJdbcConnectionString(), null, null, null, DataSources.CommitMode.DRIVER_SPECIFIED_COMMIT_MODE);
+  }
+
+  @Override
+  public ArpDialect getDialect() {
+    return ARP_DIALECT;
+  }
+
+  @VisibleForTesting
+  public static ArpDialect getDialectSingleton() {
+    return ARP_DIALECT;
+  }
+}
